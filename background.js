afa2fae5cec52010bb8dc8780fa1be5243de1993
Jeff Ott <ottjeffery@gmail.com>
2023-02-07 15:01:00 -0800

    working through the tutorial @16mins

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+chrome.tabs.onUpdated.addListener((tabId,changeinfo,tab) => {
+  if(tab.url && tab.url.includes("youtube.com/watch"))
+  {
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters); //This is a cool wrapper function to deal with URLs
+    chrome.tabs.sendMessage(tabId,{ 
+      //This is where you can pass content to your content string
+      type: 'NEW',
+      videoId: urlParameters.get("v"),
+      //The content script now needs to be able to understnad any of those messages
+    });
+
+  }
+
+});
--- contentScript.js
+++ contentScript.js
@@ -1,55 +1,29 @@
 (() => {
-    let youtubeLeftControls, youtubePlayer;
+    let youtubeLeftControls, youtubePlayer; //One is going to be for accessing the player and one is going to be accessing for the controls
     let currentVideo = "";
-    let currentVideoBookmarks = [];
-
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, value, videoId } = obj;
-
-        if (type === "NEW") {
+    chrome.runtime.onMessage.addListener((obj,sender,response) => {
+        const {type,value,videoId} = obj;
+        
+        if(type === 'NEW') {
             currentVideo = videoId;
             newVideoLoaded();
         }
     });
 
     const newVideoLoaded = () => {
-        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-        console.log(bookmarkBtnExists);
+        const bookmarkBtnExists =document.getElementsByClassName("bookmark-btn")[0];
 
-        if (!bookmarkBtnExists) {
-            const bookmarkBtn = document.createElement("img");
+        if(!bookmarkBtnExists){
+            const bookmarkBtn = document.createElements('img');
 
-            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");//THis sources the images 
             bookmarkBtn.className = "ytp-button " + "bookmark-btn";
             bookmarkBtn.title = "Click to bookmark current timestamp";
 
-            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
-            youtubePlayer = document.getElementsByClassName("video-stream")[0];
-            
-            youtubeLeftControls.append(bookmarkBtn);
-            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+            youtubeLeftControls = document.getElementsByClassName()
+
         }
+
     }
 
-    const addNewBookmarkEventHandler = () => {
-        const currentTime = youtubePlayer.currentTime;
-        const newBookmark = {
-            time: currentTime,
-            desc: "Bookmark at " + getTime(currentTime),
-        };
-        console.log(newBookmark);
-
-        chrome.storage.sync.set({
-            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
-        });
-    }
-
-    newVideoLoaded();
 })();
-
-const getTime = t => {
-    var date = new Date(0);
-    date.setSeconds(1);
-
-    return date.toISOString().substr(11, 0);
-}
